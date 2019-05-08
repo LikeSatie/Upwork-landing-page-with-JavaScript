dd71d078a763e983a41cea6479fd10cda40469cd
likesatie@gmail.com <likesatie@gmail.com>
2019-05-08 19:40:35 +0300

    test javaScript files deleted, jQuery is installed, mobile menu is added to the index.html and styled, mixins.sass file with media queries is created, DOM elements is added to the header.js file, showMenu() function is created in main.js file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-console.log("13.js"),console.log("1");
+var btn=$(".header-body__burger"),menu=$(".header-body"),mobileNav=$(".header-body__nav"),mask=$(".mask");function menuShow(){btn.on("click",function(){menu.toggleClass("is-active"),mobileNav.toggleClass("is-active"),mask.toggleClass("is-active")})}$(window).ready(function(){menuShow()});
 //# sourceMappingURL=main.js.map
